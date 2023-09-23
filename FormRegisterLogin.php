819cabc1cece751db73b73d00ec9a58c183bf694
Xevi <81320736+Xda551@users.noreply.github.com>
2023-09-23 18:18:40 +0200

    Act

--- FormRegisterLogin.php
+++ FormRegisterLogin.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>EnciamFlix!</title>
-    <link rel="icon" type="image/png" href="Imatges/enciamet.png" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/album/" />
-    <link rel="stylesheet" type="text/css" href="styleFormRL.css" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-    <script src="https://kit.fontawesome.com/02232aa1a7.js" crossorigin="anonymous"></script>
-</head>
-
+<?php include "Head.php";?>
 <body>
 
     <div class="container" id="container">
@@ -71,3 +49,4 @@
         });
     </script>
 </body>
+</html> 
